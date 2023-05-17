a33fad10363a3a74ea4444e906653078a1bc7254
BreezeBM <seyya77@gmail.com>
2023-05-17 14:50:03 +0900

    feat: 18강 DB에 저장된 정보에 접근하고, for문을 사용함

--- breezebm/accountapp/views.py
+++ breezebm/accountapp/views.py
@@ -1,9 +1,8 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from accountapp.models import HelloWorld
-
-
-# Create your views here.
 
 
 def hello_world(request):
@@ -14,6 +13,9 @@ def hello_world(request):
         new_hello_world.text = temp
         new_hello_world.save()
 
-        return render(request, "accountapp/hello_world.html", context={"hello_world_output": new_hello_world})
+        # return render(request, "accountapp/hello_world.html", context={"hello_world_list": hello_world_list})
+        return HttpResponseRedirect(reverse("accountapp:hello_world"))
     else:
-        return render(request, "accountapp/hello_world.html", context={"text": "GET METHOD"})
+        hello_world_list = HelloWorld.objects.all()
+
+        return render(request, "accountapp/hello_world.html", context={"hello_world_list": hello_world_list})

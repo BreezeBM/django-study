67bcd5bd5a29d97b703b17effb7faf6b99a8b312
BreezeBM <seyya77@gmail.com>
2023-05-23 16:06:02 +0900

    feat: 33강 리팩토링

--- breezebm/profileapp/views.py
+++ breezebm/profileapp/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
@@ -17,7 +16,6 @@ class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = "target_profile"
     form_class = ProfileCreationForm
-    success_url = reverse_lazy("accountapp:hello_world")
     template_name = "profileapp/create.html"
 
     def form_valid(self, form):
@@ -27,6 +25,10 @@ class ProfileCreateView(CreateView):
 
         return super().form_valid(form)
 
+    # self.object가 가르키는 건 Profile
+    def get_success_url(self):
+        return reverse("accountapp:detail", kwargs={"pk": self.object.user.pk})
+
 
 @method_decorator(profile_ownership_required, "get")
 @method_decorator(profile_ownership_required, "post")
@@ -34,5 +36,7 @@ class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = "target_profile"
     form_class = ProfileCreationForm
-    success_url = reverse_lazy("accountapp:hello_world")
     template_name = "profileapp/update.html"
+
+    def get_success_url(self):
+        return reverse("accountapp:detail", kwargs={"pk": self.object.user.pk})

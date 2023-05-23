9309531fd9ae153da1512e52fddd53167b138576
BreezeBM <seyya77@gmail.com>
2023-05-23 15:52:39 +0900

    feat: 32강 ProfileApp Update View 작성

--- breezebm/profileapp/views.py
+++ breezebm/profileapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, UpdateView
 
+from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
@@ -9,6 +11,8 @@ from profileapp.models import Profile
 # Create your views here.
 
 
+@method_decorator(profile_ownership_required, "get")
+@method_decorator(profile_ownership_required, "post")
 class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = "target_profile"
@@ -22,3 +26,13 @@ class ProfileCreateView(CreateView):
         temp_profile.save()
 
         return super().form_valid(form)
+
+
+@method_decorator(profile_ownership_required, "get")
+@method_decorator(profile_ownership_required, "post")
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    context_object_name = "target_profile"
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy("accountapp:hello_world")
+    template_name = "profileapp/update.html"
